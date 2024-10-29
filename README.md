a44c3e4e7688c1952b838e778f272608505d45da
Matt Thompson <matt@boondocker.io>
2024-10-29 16:59:20 -0700

    Initial import

---
+++ README.md
@@ -0,0 +1,42 @@
+# Snowcap Viewer
+
+Viewer binary for [snowcap](https://github.com/boondocklabs/snowcap/) markup files.
+
+Here's an example of how the `test.iced` file renders.
+
+<img width="1362" alt="Screenshot 2024-09-27 at 7 56 55 PM" src="https://github.com/user-attachments/assets/42a627a0-1329-4b74-93ff-bd0c9c3143b1">
+
+
+## Grammar
+
+The grammar is specified in [snowcap.pest](src/snowcap.pest) and  an example layout is in [test.iced](samples/test.iced).
+
+
+|Iced Element   | Snowcap Syntax |
+|---------------|---------------------|
+| Container     | `{<attr:val,...> ...}`|
+| Row		| `-<attr:val,...>[ element, ...]`
+| Column	| `\|<attr:val,...>[ element, ...]`
+| Stack   | `^<attr:val,...>[ element, ...]`
+| Rule (horiz)  | `rule-horizontal<height:2>()`
+| Rule (vert)   | `rule-vertical<width:2>()`
+| Text          | `text<attr:val,...>("Content")`
+| Button        | `button<attr:val,...>(element)`
+| Toggler       | `toggler<attr:val,...>(element)`
+| QRCode	| `qrcode<cell-size:10>(qr!("https://iced.rs"))`
+| Markdown      | `markdown(file!("README.md"))`
+| Image         | `image(file!("samples/ferris.png"))`
+| Svg           | `svg(file!("samples/coder.svg"))`
+
+For example, creating a container with a column would look like
+
+```
+{<width:fill, align-x:center>
+	|<align:center>[
+		text<size:19>("Hello"),
+		text<size:24>("Snowcap")
+	]
+}
+```
+
+<img width="537" alt="Screenshot 2024-09-25 at 8 36 26 PM" src="https://github.com/user-attachments/assets/db014468-8e9a-46c7-b7ee-d8e418077ce6">
---
+++ src/main.rs
@@ -0,0 +1,115 @@
+use std::process::exit;
+
+use colored::Colorize as _;
+use snowcap::iced::{Element, Task, Theme};
+use snowcap::Snowcap;
+use tracing_subscriber::{self, EnvFilter};
+
+pub fn main() -> snowcap::iced::Result {
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .with_file(true)
+        .with_line_number(true)
+        .init();
+
+    let args: Vec<String> = std::env::args().collect();
+
+    let filename = match args.get(1) {
+        Some(filename) => filename.clone(),
+        None => {
+            println!("{}", "Snowcap markfup file not provided".red());
+            exit(1)
+        }
+    };
+
+    #[cfg(feature = "profile-with-puffin")]
+    let _puffin_server = {
+        println!("Puffin Profiling Enabled. Starting server.");
+        let server_addr = format!("127.0.0.1:{}", puffin_http::DEFAULT_PORT);
+        let _puffin_server = puffin_http::Server::new(&server_addr).unwrap();
+
+        profiling::puffin::set_scopes_on(true);
+        _puffin_server
+    };
+
+    #[cfg(feature = "profile-with-tracy")]
+    {
+        println!("Tracy Profiling Enabled");
+        tracy_client::Client::start();
+    }
+
+    profiling::register_thread!("Main Thread");
+
+    snowcap::iced::application("Snowcap", SnowcapViewer::update, SnowcapViewer::view)
+        .theme(SnowcapViewer::theme)
+        .run_with(move || {
+            let viewer = SnowcapViewer::new(filename.clone());
+
+            match viewer {
+                Ok(mut viewer) => {
+                    let init_task = viewer.init();
+                    (
+                        // Provide initial state and init task
+                        viewer, init_task,
+                    )
+                }
+                Err(err) => {
+                    tracing::error!("{}", err);
+                    exit(-1)
+                }
+            }
+        })
+}
+
+struct SnowcapViewer {
+    filename: String,
+    snow: Snowcap<Message>,
+}
+
+impl SnowcapViewer {
+    pub fn new(filename: String) -> Result<Self, snowcap::Error> {
+        let mut viewer = Self {
+            filename,
+            snow: Snowcap::new().unwrap(),
+        };
+
+        viewer.load()?;
+
+        Ok(viewer)
+    }
+
+    pub fn load(&mut self) -> Result<(), snowcap::Error> {
+        self.snow.load_file(self.filename.clone())?;
+        Ok(())
+    }
+
+    fn init(&mut self) -> Task<snowcap::Message<Message>> {
+        self.snow.init()
+    }
+}
+
+#[derive(Debug, Clone)]
+enum Message {}
+
+impl SnowcapViewer {
+    #[profiling::function]
+    fn update(
+        &mut self,
+        mut message: snowcap::Message<Message>,
+    ) -> Task<snowcap::Message<Message>> {
+        self.snow.update(&mut message)
+    }
+
+    #[profiling::function]
+    fn view(&self) -> Element<snowcap::Message<Message>> {
+        profiling::scope!("View");
+        let element = self.snow.view();
+        profiling::finish_frame!();
+        element
+    }
+
+    fn theme(&self) -> Theme {
+        //Theme::TokyoNight
+        Theme::CatppuccinFrappe
+    }
+}
